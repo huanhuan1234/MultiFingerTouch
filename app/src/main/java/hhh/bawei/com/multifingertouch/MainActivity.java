3d9c64b0e8d7eb51f9430fe2c21ad11a125327a5
huanhuan1234 <905136363@qq.com>
2017-06-07 15:11:18 +0800

    Initial commit

---
+++ app/src/main/java/hhh/bawei/com/multifingertouch/MainActivity.java
@@ -0,0 +1,23 @@
+package hhh.bawei.com.multifingertouch;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.go).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,ImageViewerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
